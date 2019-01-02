f0f4187a0f0b947aa9db6f493cfbf7cdae74b4e0
ih5-tande <511508730@qq.com>
2019-01-02 15:15:51 +0800

    success

--- moveElement.js
+++ moveElement.js
@@ -1,7 +1,9 @@
 function moveElement(elementID,final_x,final_y,interval)
 {
     var elem=document.getElementById(elementID);
-
+    if(elem.movement){
+        clearTimeout(elem.movement)
+    }
     var x_pos=parseInt(elem.style.left);
     var y_pos=parseInt(elem.style.top);
     if(x_pos<final_x){
@@ -17,11 +19,9 @@ function moveElement(elementID,final_x,final_y,interval)
     {
         y_pos--;
     }
-    if(y_pos==final_y&&x_pos==final_x) return true;
+    if(y_pos===final_y&&x_pos===final_x) return true;
     elem.style.left=x_pos+"px";
     elem.style.top=y_pos+"px";
     var repeat="moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
-    movemont=setTimeout(repeat,interval);
-
-
+    elem.movement=setTimeout(repeat,interval);
 }
